8fa9d9f0f578f155cef2854f0f2c5dbb0e9b6151
Zach <zach@cuddlyspork.com>
2019-01-11 10:17:28 -0800

    Initial Commit

---
+++ LBTA_Stretch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBTA_Stretch
+//
+//  Created by Zachary Hagman on 1/11/19.
+//  Copyright © 2019 Spork and Toast, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
