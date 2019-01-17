045a23ffe029a3ea4c0d86e54d44ccd502c5845c
Zach <zach@cuddlyspork.com>
2019-01-17 14:00:02 -0800

    "Search" and "Add" buttons added

--- LBTA_Stretch/HeaderView.swift
+++ LBTA_Stretch/HeaderView.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class HeaderView: UICollectionReusableView {
-	let mainTitle: SPXLabel = {
+	let libraryName: SPXLabel = {
 		let lbl = SPXLabel(title: "My Library", color: .clear, colorOfLabelText: .white)
 		lbl.font = UIFont(name: Fonts.avenirNextBold, size: 30)
 		return lbl
+	}()
+
+	let btnStackView: SPXStackView = {
+		let stack = SPXStackView(axisLayout: .horizontal)
+		return stack
 	}()
 
 	let imageView: SPXImageView = {
@@ -21,22 +26,47 @@ class HeaderView: UICollectionReusableView {
 		return imgView
 	}()
 
+	let addButton: UIButton = {
+		let btn = UIButton()
+		btn.translatesAutoresizingMaskIntoConstraints = false
+//		btn.backgroundColor = UIColor.red
+		btn.setImage(UIImage(named: "icon-add"), for: .normal)
+		btn.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+		return btn
+	}()
+
+	let searchButton: UIButton = {
+		let btn = UIButton()
+		btn.setImage(UIImage(named: "icon-search"), for: .normal)
+		btn.translatesAutoresizingMaskIntoConstraints = false
+		return btn
+	}()
 
 	override init(frame: CGRect) {
 		super.init(frame: frame)
-
 		setupHeaderView()
 		setupVisualEffectBlur()
 	}
 
 	private func setupHeaderView() {
+		//MARK: - addSubviews
+		addSubview(imageView)
 		backgroundColor = UIColor(hex: 0xF6F7F8)
 		imageView.image = UIImage(named: "cuteHedgehog")?.resize(size: CGSize(width: self.frame.width, height: self.frame.height))
-		addSubview(imageView)
-		imageView.fillSuperview()
 
-		addSubview(mainTitle)
-		mainTitle.anchor(top: nil, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 15, bottom: 15, right: 0))
+		addSubview(libraryName)
+		addSubview(btnStackView)
+		btnStackView.addArrangedSubview(searchButton)
+		btnStackView.addArrangedSubview(addButton)
+
+		//MARK: = HeaderView - Anchors/Layout Constraints
+		imageView.fillSuperview()
+		libraryName.anchor(top: nil, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 15, bottom: 15, right: 0))
+		btnStackView.anchor(top: nil, leading: self.trailingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: .init(top: 15, left: -100, bottom: 15, right: 15), size: CGSize(width: btnStackView.frame.width, height: 35))
+
+		//MARK: = Button Targets
+		addButton.addTarget(self, action: #selector(self.addABookButtonPressed(_:)), for: .touchUpInside)
+		searchButton.addTarget(self, action: #selector(self.searchButtonPressed(_:)), for: .touchUpInside)
 	}
 
 	var animator: UIViewPropertyAnimator!
@@ -51,6 +81,15 @@ class HeaderView: UICollectionReusableView {
 
 	}
 
+	@objc func addABookButtonPressed(_ sender: UIButton) {
+		print("AddABookButtonPressed")
+		//TODO: - Build Add Book Workflow
+	}
+
+	@objc func searchButtonPressed(_ sender: UIButton) {
+		print("SearchButtonPressed")
+		//TODO: - build Search Workflow
+	}
 
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
